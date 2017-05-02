ab04de5f0aaceb5a7d7ed9935a9e0bd4050742cd
Hannu Parviainen <hannu@iac.es>
2017-05-02 14:37:29 +0100

    Moving files around.

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from numpy.distutils.core import setup, Extension
 from numpy.distutils.misc_util import Configuration
 import distutils.sysconfig as ds
 
-setup(name='CBVshrink',
-      version='0.5',
+setup(name='OxKeplerSC',
+      version='0.9',
       description='Systematics correction for Kepler light curves using PDC-MAP CBVs with Variational Bayes and shrinkage priors',
       author='Suzanne Aigrain',
       author_email='suzanne.aigrain@gmail.com',
-      url='https://github.com/saigrain/CBVshrink',
-      package_dir={'cbvshrink':'src'},
-      scripts=['bin/cbvshrink'],
-      packages=['cbvshrink'],
+      url='https://github.com/OxES/OxKeplerSC',
+      package_dir={'oxksc':'src'},
+      scripts=['bin/keplerjc','bin/cbvshrink'],
+      packages=['oxksc'],
+      ext_modules=[Extension('keplerjc.fmodels', ['src/models.f90'], libraries=['gomp','m'])],
       install_requires=['numpy', 'scipy', 'astropy']
      )
-
+    
